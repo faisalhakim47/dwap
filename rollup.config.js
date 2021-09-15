1b3b640beeca4438df9ef04def389b56d9ecac48
Faisal Hakim <faisalhakim47@gmail.com>
2021-09-15 13:42:54 +0700

    types

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,21 @@
 import { defineConfig } from 'rollup';
 import { terser } from 'rollup-plugin-terser';
 
-export default defineConfig({
-    input: './dist/index.js',
-    context: 'window',
-    plugins: [
-        terser({
-            compress: true,
-            mangle: true,
-        }),
-    ],
-    output: {
-        file: './dist/index.browser.js',
-        format: 'umd',
-        name: 'Dwap',
-        sourcemap: true,
+export default defineConfig([
+    {
+        input: './dist/index.js',
+        context: 'window',
+        plugins: [
+            terser({
+                compress: true,
+                mangle: true,
+            }),
+        ],
+        output: {
+            file: './dist/index.browser.js',
+            format: 'iife',
+            name: 'Dwap',
+            sourcemap: true,
+        },
     },
-});
+]);
